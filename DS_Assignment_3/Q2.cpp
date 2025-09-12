bca40b790433d79aef0812150b8cc112856f9cc1
ishaangupta122 <ishaang2209@gmail.com>
2025-09-12 14:19:32 +0530

    Update 1.3

--- DS_Assignment_3/Q2.cpp
+++ DS_Assignment_3/Q2.cpp
@@ -10,61 +10,61 @@ char stackArr[MAX];
 int top = -1;
 
 int isEmpty() {
-    return (top < 0);
+  return (top < 0);
 }
 
 int isFull() {
-    return (top >= MAX - 1);
+  return (top >= MAX - 1);
 }
 
 void push(char x) {
-    if (isFull()) {
-        cout << "Stack Overflow! Cannot push " << x << endl;
-    } else {
-        stackArr[++top] = x;
-    }
+  if (isFull()) {
+    cout << "Stack Overflow! Cannot push " << x << endl;
+  } else {
+    stackArr[++top] = x;
+  }
 }
 
 int pop() {
-    if (isEmpty()) {
-        cout << "Stack Underflow! Cannot pop." << endl;
-        return -1;
-    } else {
-        return stackArr[top--];
-    }
+  if (isEmpty()) {
+    cout << "Stack Underflow! Cannot pop." << endl;
+    return -1;
+  } else {
+    return stackArr[top--];
+  }
 }
 
 int main() {
-    char val;
-    int n;
-    int size = (sizeof(stackArr) / sizeof(stackArr[0]));
+  char val;
+  int n;
+  int size = (sizeof(stackArr) / sizeof(stackArr[0]));
 
-    cout << "\nEnter how many elements you want to insert: ";
-    cin >> n;
-    cout << "Enter characters into stack separated by space: ";
+  cout << "\nEnter how many elements you want to insert: ";
+  cin >> n;
+  cout << "Enter characters into stack separated by space: ";
 
-    for (int i = 0; i < n; i++) {
-        cin >> val;
-        push(val);
-    }
+  for (int i = 0; i < n; i++) {
+    cin >> val;
+    push(val);
+  }
 
-    cout << "\nOriginal Stack: ";
-    for (int k = 0; k < size; k++) {
-        cout << stackArr[k];
-    }
+  cout << "\nOriginal Stack: ";
+  for (int k = 0; k < size; k++) {
+    cout << stackArr[k];
+  }
 
-    cout << "\nReversed Stack with reverse loop: ";
-    for (int j = top; j >= 0; j--) {
-        cout << stackArr[j];
-    }
+  cout << "\nReversed Stack with reverse loop: ";
+  for (int j = top; j >= 0; j--) {
+    cout << stackArr[j];
+  }
 
-    cout << "\nReversed Stack with popping: ";
-    while (!isEmpty()) {
-        cout << stackArr[top];
-        pop();
-    }
+  cout << "\nReversed Stack with popping: ";
+  while (!isEmpty()) {
+    cout << stackArr[top];
+    pop();
+  }
 
-    cout << endl;
+  cout << endl;
 
-    return 0;
+  return 0;
 }
--- DS_Assignment_3/Q3.cpp
+++ DS_Assignment_3/Q3.cpp
@@ -4,7 +4,35 @@
 using namespace std;
 
 int main() {
-    cout << "Hello";
+  char expr[100];
+  cout << "Enter the expression: ";
+  cin >> expr;
 
-    return 0;
+  char stack[100];
+  int top = -1;
+  bool balanced = true;
+
+  for (int i = 0; expr[i] != '\0'; i++) {
+    char ch = expr[i];
+    if (ch == '(' || ch == '{' || ch == '[') {
+      stack[++top] = ch;
+    } else if (ch == ')' || ch == '}' || ch == ']') {
+      if (top == -1) {
+        balanced = false;
+        break;
+      }
+      char open = stack[top--];
+      if ((ch == ')' && open != '(') || (ch == '}' && open != '{') || (ch == ']' && open != '[')) {
+        balanced = false;
+        break;
+      }
+    }
+  }
+
+  if (balanced && top == -1)
+    cout << "Balanced parentheses." << endl;
+  else
+    cout << "Unbalanced parentheses." << endl;
+
+  return 0;
 }
