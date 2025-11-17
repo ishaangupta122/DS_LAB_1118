b8aafe3fe0fa4a7242c1cb61883b4bf6778a3288
ishaangupta122 <ishaang2209@gmail.com>
2025-11-17 17:14:51 +0530

    pending assignments (6, 8, 9) & extra ques of all.

---
+++ DS_Assignment_8/Q1.cpp
@@ -0,0 +1,2 @@
+// 1. Write program using functions for binary tree traversals: Pre-order, In-order and Post
+// order using recursive approach.
---
+++ DS_Assignment_8/Q2.cpp
@@ -0,0 +1,6 @@
+// 2. Implement following functions for Binary Search Trees
+// (a) Search a given item (Recursive & Non-Recursive)
+// (b) Maximum element of the BST
+// (c) Minimum element of the BST
+// (d) In-order successor of a given node the BST
+// (e) In-order predecessor of a given node the BST
